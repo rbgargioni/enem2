e382566a865f0c71d1277a62f547cd1546182f24
Rafael Bueno Gargioni <113815761+rbgargioni@users.noreply.github.com>
2023-07-13 16:19:23 -0300

    index.js

--- js/index.js
+++ js/index.js
@@ -2,5 +2,14 @@ const btnMobile = document.getElementById('btn-mobile');
 function toggleMenu(){
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
-}
+    
+    const btnvolume1 = document.getElementById('volume1');
+        function toggleMenu(){
+            const nav2 = document.getElementById('nav2');
+            nav2.classList.toggle('active2'); 
+        }
+        btnvolume1.addEventListener('click', toggleMenu);
+}   
+
 btnMobile.addEventListener('click', toggleMenu);
+
