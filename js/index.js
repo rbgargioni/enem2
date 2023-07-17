1680d07ebb26f400b3cf4e985c82704118bd7e0c
Rafael Bueno Gargioni <113815761+rbgargioni@users.noreply.github.com>
2023-07-17 18:19:23 -0300

    index.js

--- js/index.js
+++ js/index.js
@@ -8,32 +8,32 @@
 function clickMenu(){
     if (menu.style.display == 'block'){
         menu.style.display = 'none'
-        menu2.style.display = 'none'
-        menu3.style.display = 'none'
     }
     else{
         menu.style.display = 'block'
+        menu2.style.display = 'none'
+        menu3.style.display = 'none'
     }
 }
  
 function clickMenu2(){
     if (menu2.style.display == 'block'){
-        menu.style.display = 'none'
         menu2.style.display = 'none'
-        menu3.style.display = 'none'
     }
     else{
+        menu.style.display = 'none'
         menu2.style.display = 'block'
+        menu3.style.display = 'none'
     }
 }
 
 function clickMenu3(){
     if (menu3.style.display == 'block'){
         menu3.style.display = 'none'
-        menu.style.display = 'none'
-        menu2.style.display = 'none'
     }
     else{
+        menu.style.display = 'none'
+        menu2.style.display = 'none'
         menu3.style.display = 'block'
     }
 }
