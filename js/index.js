cba73722162a5a90def092e5412bdec28ae47df0
Rafael Bueno Gargioni <rbgargioni2@gmail.com>
2024-03-03 20:54:42 -0300

    Acabando os  links

--- js/index.js
+++ js/index.js
@@ -24,8 +24,69 @@ function verificarFragmento() {
     }
 }
 // função de cliques para compartilhamento
+// Artes//
 function executarArtesano1() { clickMenu1(); artesvolume1();} 
-function executarArtesano2() { clickMenu1(); artesvolume2();} 
+function executarArtesano2() { clickMenu1(); artesvolume2();}
+function executarArtesano3() { clickMenu1(); artesvolume3();} 
+///BIOLOGIA
+function executarbiologiano1() { clickMenu2(); biologiaAno1();} 
+function executarbiologiano2() { clickMenu2(); biologiaAno2();} 
+function executarbiologiano3() { clickMenu2(); biologiaAno3();} 
+///Educação fisica
+function executaredFisicano1() { clickMenu3(); edFisicaAno1();}
+function executaredFisicano2() { clickMenu3(); edFisicaAno2();}
+function executaredFisicano3() { clickMenu3(); edFisicaAno3();}
+///Educação Espanhol
+function executarespanhol_ano1() { clickMenu4(); espanholAno1();}
+function executarespanhol_ano2() { clickMenu4(); espanholAno2();}
+///Educação FILOSOFIA
+function executarfiloAno1() { clickMenu5(); filosofiaAno1();}
+function executarfiloAno2() { clickMenu5(); filosofiaAno2();}
+function executarfiloAno3() { clickMenu5(); filosofiaAno3();}
+///Educação FISICA
+function executarfisicaAno1() { clickMenu6(); fisiAno1();}
+function executarfisicaAno2() { clickMenu6(); fisiAno2();}
+function executarfisicaAno3() { clickMenu6(); fisiAno3();}
+///Educação GEOGRAFIA
+function executargeografiaAno1() { clickMenu7(); geoAno1();}
+function executargeografiaAno2() { clickMenu7(); geoAno2();}
+function executargeografiaAno3() { clickMenu7(); geoAno3();}
+///Educação HISTORIA
+function executarhistoriaAno1() { clickMenu8(); histAno1();}
+function executarhistoriaAno2() { clickMenu8(); histAno2();}
+function executarhistoriaAno3() { clickMenu8(); histAno3();}
+///Educação INGLÊS
+function executaringlesAno1() { clickMenu9(); ingAno1();}
+function executaringlesAno2() { clickMenu9(); ingAno2();}
+function executaringlesAno3() { clickMenu9(); ingAno3();}
+///Educação LITERATURA
+function executarliteraturaAno1() { clickMenu10(); litAno1();}
+function executarliteraturaAno2() { clickMenu10(); litAno2();}
+function executarliteraturaAno3() { clickMenu10(); litAno3();}
+///Educação MATEMÁTICA
+function executarmatematicaAno1() { clickMenu11(); matAno1();}
+function executarmatematicaAno2() { clickMenu11(); matAno2();}
+function executarmatematicaAno3() { clickMenu11(); matAno3();}
+///Educação Português
+function executarportuguesAno1() { clickMenu12(); porAno1();}
+function executarportuguesAno2() { clickMenu12(); porAno2();}
+///Educação QUÍMICA
+function executarquimicaAno1() { clickMenu13(); quimAno1();}
+function executarquimicaAno2() { clickMenu13(); quimAno2();}
+function executarquimicaAno3() { clickMenu13(); quimAno3();}
+///Educação REDAÇÃO
+function executaredacaoAno1() { clickMenu14(); redAno1();}
+function executaredacaoAno2() { clickMenu14(); redAno2();}
+function executaredacaoAno3() { clickMenu14(); redAno3();}
+///Educação SOCIOLOGIA
+function executarsociologiaAno1() { clickMenu15(); socAno1();}
+function executarsociologiaAno2() { clickMenu15(); socAno2();}
+function executarsociologiaAno3() { clickMenu15(); socAno3();}
+
+
+
+
+
 
 
 ///////////////////// Menu de Artes////////////////////////////// 
@@ -283,6 +344,134 @@ function litAno3(){
     literaturaAno3.style.display = "block";
     aulasliteratura.style.display = "none";
 }
+
+///////////////////// Menu de Matematica//////////////////////////////
+function clickMenu11() {
+    var menuMate = document.getElementById("MATEMATICA");
+    var menuPrincipal = document.getElementById("menuprincipal");
+    // Verifica se o menu está visível
+    var isVisible = window.getComputedStyle(menuMate).display !== "none";
+
+    // Alterna a visibilidade do menu
+    menuMate.style.display = isVisible ? "none" : "block";
+    menuprincipal.style.display = "none";
+    menuMate.style.display = "block";
+}
+
+function matAno1(){
+    matematicaAno1.style.display = "block";
+    aulasmatematica.style.display = "none";
+}
+function matAno2(){
+    matematicaAno2.style.display = "block";
+    aulasmatematica.style.display = "none";
+}
+function matAno3(){
+    matematicaAno3.style.display = "block";
+    aulasmatematica.style.display = "none";
+}
+
+///////////////////// Menu de Portugues//////////////////////////////
+function clickMenu12() {
+    var menuPor = document.getElementById("PORTUGUES");
+    var menuPrincipal = document.getElementById("menuprincipal");
+    // Verifica se o menu está visível
+    var isVisible = window.getComputedStyle(menuPor).display !== "none";
+
+    // Alterna a visibilidade do menu
+    menuPor.style.display = isVisible ? "none" : "block";
+    menuprincipal.style.display = "none";
+    menuPor.style.display = "block";
+}
+
+function porAno1(){
+    portuguesAno1.style.display = "block";
+    aulasPORTUGUES.style.display = "none";
+}
+function porAno2(){
+    portuguesAno2.style.display = "block";
+    aulasPORTUGUES.style.display = "none";
+}
+
+///////////////////// Menu de QUIMICA//////////////////////////////
+function clickMenu13() {
+    var menuquim = document.getElementById("QUIMICA");
+    var menuPrincipal = document.getElementById("menuprincipal");
+    // Verifica se o menu está visível
+    var isVisible = window.getComputedStyle(menuquim).display !== "none";
+
+    // Alterna a visibilidade do menu
+    menuquim.style.display = isVisible ? "none" : "block";
+    menuprincipal.style.display = "none";
+    menuquim.style.display = "block";
+}
+
+function quimAno1(){
+    quimicaAno1.style.display = "block";
+    aulasquimica.style.display = "none";
+}
+function quimAno2(){
+    quimicaAno2.style.display = "block";
+    aulasquimica.style.display = "none";
+}
+function quimAno3(){
+    quimicaAno3.style.display = "block";
+    aulasquimica.style.display = "none";
+}
+
+///////////////////// Menu de REDAÇÂO//////////////////////////////
+function clickMenu14() {
+    var menuredacao = document.getElementById("REDACAO");
+    var menuPrincipal = document.getElementById("menuprincipal");
+    // Verifica se o menu está visível
+    var isVisible = window.getComputedStyle(menuredacao).display !== "none";
+
+    // Alterna a visibilidade do menu
+    menuredacao.style.display = isVisible ? "none" : "block";
+    menuprincipal.style.display = "none";
+    menuredacao.style.display = "block";
+}
+
+function redAno1(){
+    redacaoAno1.style.display = "block";
+    aulasredacao.style.display = "none";
+}
+function redAno2(){
+    redacaoAno2.style.display = "block";
+    aulasredacao.style.display = "none";
+}
+function redAno3(){
+    redacaoAno3.style.display = "block";
+    aulasredacao.style.display = "none";
+}
+
+///////////////////// Menu de SOCIOLOGIA//////////////////////////////
+function clickMenu15() {
+    var menusociologia = document.getElementById("SOCIOLOGIA");
+    var menuPrincipal = document.getElementById("menuprincipal");
+    // Verifica se o menu está visível
+    var isVisible = window.getComputedStyle(menusociologia).display !== "none";
+
+    // Alterna a visibilidade do menu
+    menusociologia.style.display = isVisible ? "none" : "block";
+    menuprincipal.style.display = "none";
+    menusociologia.style.display = "block";
+}
+
+function socAno1(){
+    sociologiaAno1.style.display = "block";
+    aulassociologia.style.display = "none";
+}
+function socAno2(){
+    sociologiaAno2.style.display = "block";
+    aulassociologia.style.display = "none";
+}
+function socAno3(){
+    sociologiaAno3.style.display = "block";
+    aulassociologia.style.display = "none";
+}
+
+
 
 verificarFragmento();
 
